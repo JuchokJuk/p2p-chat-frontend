a041c326042be103944059a6d866ffc704d4c491
JuchokJuk <juk.juchok@yandex.ru>
2023-12-14 22:15:07 +0300

    .

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,21 @@
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 import fs from 'fs';
+import { PUBLIC_ENVIRONMENT } from '$env/static/public';
+
+function server(environment: string) {
+	if (environment === 'DEV') {
+		return {
+			https: {
+				key: fs.readFileSync(`${__dirname}/cert/key.pem`),
+				cert: fs.readFileSync(`${__dirname}/cert/cert.pem`)
+			},
+			proxy: {}
+		};
+	}
+}
 
 export default defineConfig({
 	plugins: [sveltekit()],
-	server: {
-		https: {
-			key: fs.readFileSync(`${__dirname}/cert/key.pem`),
-			cert: fs.readFileSync(`${__dirname}/cert/cert.pem`)
-		},
-		proxy: {}
-	}
+	server: server(PUBLIC_ENVIRONMENT)
 });
